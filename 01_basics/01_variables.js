208416917420d2a210718deb437478fc0b509916
Vinayak5619 <158333224+Vinayak5619@users.noreply.github.com>
2024-11-20 14:58:32 +0000

    V ariables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=4537
+let accountEmail="vinayak@google.com"
+var accountPassword="45639"
+accountCity ="Jaipur";
+
+accountEmail="voins@gmail.com"
+accountPassword="88676"
+accountCity ="Belgaum"
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
