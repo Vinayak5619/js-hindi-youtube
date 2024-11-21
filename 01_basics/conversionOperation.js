dfc4c6155725bd4d971fdee7931283b47a4f2974
Vinayak5619 <158333224+Vinayak5619@users.noreply.github.com>
2024-11-21 13:54:48 +0000

    conversion of datetypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,25 @@
+let score="33";
+
+// console.log(typeof(score));
+
+
+// let valueInNumber =Number(score);
+
+//console.log(typeof(valueInNumber))
+
+//"33"=33
+//"33bb"=Nan
+//true=1
+
+let isLogged=1
+let booleanIsLogged=Boolean(isLogged)
+console.log(booleanIsLogged);
+
+
+let someNumber=33;
+
+let stringNumber=String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
